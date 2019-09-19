21caa00d021e6fe7bbd008dac9e3424302d3f379
krushnatkhawale@gmail.com <krushnatkhawale@gmail.com>
2019-09-19 07:02:51 +0100

    frontend: booklist component added

--- bookstore-frontend/src/app/book-list/book-list.component.ts
+++ bookstore-frontend/src/app/book-list/book-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Book } from '../service/model/Book'
+import { BookControllerService } from '../service/api/bookController.service'
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-book-list',
@@ -7,34 +10,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookListComponent implements OnInit {
 
-  nbOfBooks: number = 2
-  books = [
-     {
-          "id": 1,
-          "title": "this is book 1",
-          "description": "this is book 1",
-          "unitCost": 345.0,
-          "isbn": "ISBN-1",
-          "publicationDate": "2015-12-11",
-          "nbOfPages": 2323,
-          "imageUrl": "https://images-na.ssl-images-amazon.com/images/I/51uiOEOz01L._AC_UL115_.jpg",
-          "language": null
-     },
-     {
-          "id": 2,
-          "title": "this is book 2",
-          "description": "this is book 2",
-          "unitCost": 345.0,
-          "isbn": "ISBN-2",
-          "publicationDate": "2015-12-11",
-          "nbOfPages": 2323,
-          "imageUrl": "https://images-na.ssl-images-amazon.com/images/I/518djjKLsHL._AC_UL115_.jpg",
-          "language": null
-     }
-  ]
-  constructor() { }
+  nbOfBooks: number;
+  books: Book[];
+
+  constructor(private bookService: BookControllerService) { }
 
   ngOnInit() {
+    this.bookService.getBookCountUsingGET().subscribe( nbOfBooks => this.nbOfBooks = nbOfBooks );
+    this.bookService.getBooksUsingGET().subscribe( books => this.books = books );
   }
-
 }
